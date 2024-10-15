be78fdf4794a3792fc380406f0c086d907295afc
BandhanHardiya <150674357+BandhanHardiya@users.noreply.github.com>
2024-10-15 18:32:39 +0530

    exception handling added main.py

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
 import mysql.connector
 #change password according to your mysql password
-print("Enter your Credentials")
-hos=input("Enter Host Name:")
-passwor=input("Enter Password:")
-use=input("Enter User Name:")
-conn=mysql.connector.connect(host=hos,password=passwor,user=use,database='inventory_management_sys')
-cursorr=conn.cursor()
-
-if conn.is_connected():
+while True:
+  try:
+   print("Enter your Credentials")
+   global hos
+   hos=input("Enter Host Name:")
+   global passwor
+   passwor=input("Enter Password:")
+   global use
+   use=input("Enter User Name:")
+   conn=mysql.connector.connect(host=hos,password=passwor,user=use,database='inventory_management_sys')
+   cursorr=conn.cursor()
+  except mysql.connector.Error as err:
+    print("Error while connecting my sql")
+    print(err)
+    continue
+  except Exception as e:
+    print("Exception",e)
+    continue
+  
+  if conn.is_connected():
     print("Connection Successful")
-else:
+    break
+  else:
     print("Invalid Credentials")
 
 
