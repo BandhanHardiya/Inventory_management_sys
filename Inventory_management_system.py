2371f40cec9079824b70ad5d9bb8de7aa9adcfe5
BandhanHardiya <150674357+BandhanHardiya@users.noreply.github.com>
2024-10-15 19:18:23 +0530

    exception handling added Inventory_management_system.py

--- Inventory_management_system.py
+++ Inventory_management_system.py
@@ -5,23 +5,38 @@ import Customer
 import Orders
 import main
 main
-print("1 For Values in PRODUCT table:")
-print("2 For Values in CATEGORY table:")
-print("3 For Values in SUPPLIER table:")
-print("4 For Values in CUSTOMER table:")
-print("5 for Values in ORDERS table:")
-choice=int(input("Enter your choice:"))
+while True:
+  try:
+   print("1 For Values in PRODUCT table:")
+   print("2 For Values in CATEGORY table:")
+   print("3 For Values in SUPPLIER table:")
+   print("4 For Values in CUSTOMER table:")
+   print("5 for Values in ORDERS table:")
+   choice=int(input("Enter your choice:"))
+  except ValueError:
+      print("Input numeric value")
+      continue
+  except Exception as e:
+      print("Exception:",e)
+      continue
+  else:
+      break
+         
 
 nor=1
 while nor==1:
    
     while choice==1:
-        print("1 For Input Values in PRODUCT Table")
-        print("2 For Select Every Values from PRODUCT Table")
-        print("3 for showing details with PRODUCT ID") 
-        print("4 for Updating stock from product ID")
-        print("5 for exit")
-        l=int(input("Enter your choice:"))
+        try:
+         print("1 For Input Values in PRODUCT Table")
+         print("2 For Select Every Values from PRODUCT Table")
+         print("3 for showing details with PRODUCT ID") 
+         print("4 for Updating stock from product ID")
+         print("5 for exit")
+         l=int(input("Enter your choice:"))
+        except ValueError:
+            print("Enter numeric value")
+            continue
 
         if l==1:
             Product.pro()
@@ -36,12 +51,17 @@ while nor==1:
             
             
             
-    while choice==2:    
-        print("1 For Input Values in CATEGORY Table")
-        print("2 For Select Every Values from CATEGORY Table")
-        print("3 for showing details with CATEGORY ID") 
-        print("4 for exit")
-        l=int(input("Enter your choice:"))
+    while choice==2:
+        try:    
+         print("1 For Input Values in CATEGORY Table")
+         print("2 For Select Every Values from CATEGORY Table")
+         print("3 for showing details with CATEGORY ID") 
+         print("4 for exit")
+         l=int(input("Enter your choice:"))
+        except ValueError:
+            print("Enter numeric value")
+            continue
+
         if l==1:
             Category.cat()
         elif l==2:
@@ -52,12 +72,17 @@ while nor==1:
             choice=0
 
 
-    while choice==3:       
-        print("1 For Input Values in SUPPLIER Table")
-        print("2 For Select Every Values from SUPPLIER Table")
-        print("3 for showing details with SUPPLIER ID") 
-        print("4 for exit")
-        l=int(input("Enter your choice:"))
+    while choice==3:
+        try:       
+         print("1 For Input Values in SUPPLIER Table")
+         print("2 For Select Every Values from SUPPLIER Table")
+         print("3 for showing details with SUPPLIER ID") 
+         print("4 for exit")
+         l=int(input("Enter your choice:"))
+        except ValueError:
+            print("Enter numeric value")
+            continue
+
         if l==1:
            Supplier.sup()
         elif l==2:
@@ -68,12 +93,17 @@ while nor==1:
             choice=0
 
 
-    while choice==4:        
-        print("1 For Input Values in CUSTOMER Table")
-        print("2 For Select Every Values from CUSTOMER Table")
-        print("3 for showing details with CUSTOMER ID") 
-        print("4 for exit")
-        l=int(input("Enter your choice:"))
+    while choice==4:
+        try:        
+         print("1 For Input Values in CUSTOMER Table")
+         print("2 For Select Every Values from CUSTOMER Table")
+         print("3 for showing details with CUSTOMER ID") 
+         print("4 for exit")
+         l=int(input("Enter your choice:"))
+        except ValueError:
+           print("Enter numeric value")
+           continue
+
         if l==1:
             Customer.cus()
         elif l==2:
@@ -84,12 +114,17 @@ while nor==1:
             choice=0
 
 
-    while choice==5:        
-        print("1 For Input Values in ORDER Table")
-        print("2 For Select Every Values from ORDER Table")
-        print("3 for showing details with ORDER ID") 
-        print("4 for exit")
-        l=int(input("Enter your choice:"))
+    while choice==5:
+        try:
+         print("1 For Input Values in ORDER Table")
+         print("2 For Select Every Values from ORDER Table")
+         print("3 for showing details with ORDER ID") 
+         print("4 for exit")
+         l=int(input("Enter your choice:"))
+        except ValueError:
+           print("Enter numeric value")
+           continue
+
         if l==1:
            Orders.ord()
         elif l==2:
@@ -102,18 +137,26 @@ while nor==1:
      
     else:
         print("Invalid choice")
-
-    print("1 For Input Values in PRODUCT table:")
-    print("2 For Input Values in CATEGORY table:")
-    print("3 For Input Values in SUPPLIER table:")
-    print("4 For Input Values in CUSTOMER table:")
-    print("5 for Input Values in ORDERS table:")
-    print("6 or any number for exit")
-    choice=int(input("Enter your choice:"))
-    if choice==1 or choice==2 or choice==3 or choice==4 or choice==5:
-        nor=1
-    else:
-        nor=0
+    
+    while True:
+       try:
+        print("1 For Input Values in PRODUCT table:")
+        print("2 For Input Values in CATEGORY table:")
+        print("3 For Input Values in SUPPLIER table:")
+        print("4 For Input Values in CUSTOMER table:")
+        print("5 for Input Values in ORDERS table:")
+        print("6 or any number for exit")
+        choice=int(input("Enter your choice:"))
+       except ValueError:
+         print("Enter numeric value")
+         continue
+       
+       if choice==1 or choice==2 or choice==3 or choice==4 or choice==5:
+          nor=1
+          break
+       else:
+          nor=0
+          break
     
 print("Thanks for using Inventory Management System")
 print("Created by Bandhan Hardiya")  
