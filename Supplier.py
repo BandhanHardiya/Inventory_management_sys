c141f95973f918d11cd97b79d540c4b006be0797
BandhanHardiya <150674357+BandhanHardiya@users.noreply.github.com>
2024-10-15 18:18:05 +0530

    exception handling added Supplier.py

--- Supplier.py
+++ Supplier.py
@@ -7,18 +7,24 @@ def sup():
  print("This program is use to Enter details in Supplier Table:")
  h=1
  while h==1:
-    fromula="INSERT INTO supplier (supplierID,supplierName,address,city,postal_code,country,phoneNO) values (%s,%s,%s,%s,%s,%s,%s)"
-    supplierID=int(input("Enter Supplier ID:"))
-    supplierName=input("Enter Supplier Name:")
-    address=input("Enter address:")
-    city=input("Enter City Name:")
-    postal_code=input("Enter Postal Code:")
-    country=input("Enter country Name:")
-    phoneNO=input("Enter phone Number:")
-    values=(supplierID,supplierName,address,city,postal_code,country,phoneNO)
-    cursorr.execute(fromula,values)
-    conn.commit()
-    print("Supplier Details are inserted Succesfully")
+    try:
+     fromula="INSERT INTO supplier (supplierID,supplierName,address,city,postal_code,country,phoneNO) values (%s,%s,%s,%s,%s,%s,%s)"
+     supplierID=int(input("Enter Supplier ID:"))
+     supplierName=input("Enter Supplier Name:")
+     address=input("Enter address:")
+     city=input("Enter City Name:")
+     postal_code=input("Enter Postal Code:")
+     country=input("Enter country Name:")
+     phoneNO=input("Enter phone Number:")
+     values=(supplierID,supplierName,address,city,postal_code,country,phoneNO)
+     cursorr.execute(fromula,values)
+    except ValueError:
+      print("Enter numeric value where neede")
+    except Exception as e:
+      print("Excepion",e)
+    else:
+     conn.commit()
+     print("Supplier Details are inserted Succesfully")
     z=int(input("Enter 1 for another record and 2 for exit:"))
     if z==1:
       h=1
@@ -39,11 +45,17 @@ def sel_all():
   
 
 def sup_detl():
-  cursorr=conn.cursor()
-  supplierID=int(input("Enter Supplier ID to get details:"))
-  cursorr.execute("select * from supplier where supplierID=%s",(supplierID,))
-  result=cursorr.fetchone()
-  if result:
+  while True:
+   try:
+    cursorr=conn.cursor()
+    supplierID=int(input("Enter Supplier ID to get details:"))
+    cursorr.execute("select * from supplier where supplierID=%s",(supplierID,))
+    result=cursorr.fetchone()
+   except ValueError:
+    print("Enter numecric value")
+   except Exception as e:
+    print("Excepion",e)
+   if result:
     print("Supplier ID:",result[0])
     print("Supplier Name:",result[1])
     print("Supplier Address:",result[2])
@@ -51,7 +63,10 @@ def sup_detl():
     print("Postal Code:",result[4])
     print("Country:",result[5])
     print("Phone Number:",result[6])
-    z=int(input("Enter 1 if you want details of another Supplier or any number for exit:"))
-    if z==1:
-      sup_detl()
+   else:
+    print("Supplier not found")
+
+   z=int(input("Enter 1 if you want details of another Supplier or any number for exit:"))
+   if z!=1:
+     break
   
